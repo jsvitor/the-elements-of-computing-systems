670415faaca8751ad76bd984833482f261911f78
Vitor <jszvitor@gmail.com>
2021-11-03 20:25:07 -0300

    feat(main): added solution of project 4

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,49 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// Put your code here. josé vitor
+(INICIO)
+  // @24576 = RAM[24576] = keyboard = teclado
+  @24576
+  D = A
+  @teclado
+  M = D
+(VERIFICA_TECLADO)
+  // Coloca o valor atual para o último pixel da tela mapeado
+  @24575
+  D = A
+  @atual
+  M = D // RAM[atual] = 24575
+  // se o teclado for pressionado, será diferente de 0.
+  // Então preencha a tela com branco.
+  @teclado
+  A = M
+  D = M
+  @valor_de_preenchimento
+  M = -1
+  @PINTE
+  D;JNE
+  // se não, limpe a tela
+  @valor_de_preenchimento
+  M = 0
+(PINTE)
+  // Preenha ou limpe o pixel atual, dependendo do valor_de_preenchimento
+  @valor_de_preenchimento
+  D = M
+  @atual
+	A = M
+	M = D
+	// Se for o primeiro pixel, não resta nada pra pintar,
+  // então volte para o verificador do teclado
+	@atual
+	D = M
+	@16384
+	D = D - A
+	@VERIFICA_TECLADO
+	D;JLE
+	// Decrementa o pixel atual
+	@atual
+	M = M - 1
+	// continue "pintando" o próximo pixel.
+	@PINTE
+	0;JMP
